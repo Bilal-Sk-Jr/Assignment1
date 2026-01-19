54092aea13f99c06cfed7c0ea330b32ea2d05d4a
Bilal <bilalsajjadkhan@gmail.com>
2026-01-19 16:10:52 +0500

    Update Even Numbers Output.java

--- Even Numbers Output.java
+++ Even Numbers Output.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class Even_Num_Printing {
-    public static void main(String[] args) {
+public class Even_Num_Printing{
+    public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
 
         int StartVal, EndVal;
-        int Count;
+        int Num;
         System.out.println("Enter starting number: ");
 
-        while (input.hasNextInt() == false) {
+        while (input.hasNextInt() == false){
             System.out.println("Enter a INTEGER number: ");
             input.next();
         }
@@ -16,20 +16,28 @@ public class Even_Num_Printing {
 
         System.out.println("Enter ending number: ");
 
-        while (input.hasNextInt() == false) {
+        while (input.hasNextInt() == false){
             System.out.println("Enter a INTEGER number: ");
             input.next();
         }
         EndVal = input.nextInt();
 
-        Count = StartVal;
-        while (Count <= EndVal) {
-            if (Count % 2 == 0) {
-                System.out.println(Count);
-                Count = Count + 1;
-            } else {
-                Count = Count + 1;
+        Num = StartVal;
+        
+        if (Num % 2 == 0){
+            while (Num <= EndVal){
+                System.out.println(Num);
+                Num = Num + 2;
             }
         }
+
+        else{
+            Num = Num + 1;
+            while (Num <= EndVal){
+                System.out.println(Num);
+                Num = Num + 2;
+            }
+        }
+        input.close();
     }
 }
